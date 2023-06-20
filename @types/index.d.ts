c7571f34212a79fb64b3dac871d956e5679328a1
Vinícius Gutierrez <srgutyerrez@gmail.com>
2023-06-19 23:21:59 -0300

    fix: duplicate text on class name

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,5 +1,5 @@
 declare module '@gentifly/exceptions' {
-  export class ExceptionException extends Error {
+  export class Exception extends Error {
     public readonly status: number;
 
     constructor(name: string, message: string, status: number)
